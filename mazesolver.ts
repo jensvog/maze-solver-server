7219392a21d8b0a9752d07fae380f85aaf826817
jvog <62656589+jensvog@users.noreply.github.com>
2020-04-16 20:31:34 +0200

    Adds unit test environment and unit test
    Unit test environment with mocha and chai set up. Create unit test and
    prepare test run routine.

--- mazesolver.ts
+++ mazesolver.ts
@@ -1,10 +1,10 @@
 var createPlanner = require('l1-path-finder');
 
-function planPath(startX: number, startY: number, goalX: number, goalY: number, maze: any) {
-    //Create path planner
+export function planPath(startX: number, startY: number, goalX: number, goalY: number, maze: any) {
+    // Create path planner
     var planner = createPlanner(maze);
 
-    //Find path
+    // Find path
     var path = [];
     planner.search(startX, startY, goalX, goalY, path);
 
---
+++ test/test.ts
@@ -0,0 +1,23 @@
+import { planPath } from '../mazesolver';
+import { expect } from 'chai';
+import 'mocha'
+
+var ndarray = require('ndarray');
+
+describe('planPath function', () => {
+  it('should calculate a correct path', function() {
+      var maze = ndarray([
+          0, 1, 0, 0, 0, 0, 0,
+          0, 1, 0, 1, 0, 0, 0,
+          0, 1, 0, 1, 1, 1, 0,
+          0, 1, 0, 1, 0, 0, 0,
+          0, 1, 0, 1, 0, 0, 0,
+          0, 1, 0, 1, 0, 0, 0,
+          0, 1, 0, 1, 0, 1, 1,
+          0, 0, 0, 1, 0, 0, 0,
+        ], [8, 7])
+
+      var path = planPath(0, 0, 7, 6, maze);
+      expect(path).eql([ 0, 0, 7, 0, 7, 2, 0, 2, 0, 4, 1, 4, 1, 6, 3, 6, 5, 6, 5, 4, 7, 4, 7, 6 ]);
+  });
+});
